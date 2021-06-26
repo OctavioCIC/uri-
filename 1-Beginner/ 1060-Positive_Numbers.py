e7422f419a61196d18b0d9ef4170b26c7bd2786f
Octavio Augusto <85756926+octav1oaugusto@users.noreply.github.com>
2021-06-26 09:21:03 -0300

    Delete 1060-Positive_Numbers.py

--- 1-Beginner/ 1060-Positive_Numbers.py
+++
@@ -1,8 +0,0 @@
-# https://www.urionlinejudge.com.br/judge/en/problems/view/1060
-
-cont = 0
-for _ in range(6):
-    num = float(input())
-    if num > 0:
-        cont += 1
-print(f'{cont} valores positivos')
