e1608d5423c14dacc49109a42e1495ecc5ea7ab8
octav1oaugusto <octavioaugusto.cs@hotmail.com>
2021-07-23 18:50:19 -0300

    Adicionar novo exercício

---
+++ 1-Beginner/1186 - Below_the_Secundary_Diagonal.py
@@ -0,0 +1,21 @@
+# https://www.urionlinejudge.com.br/judge/en/problems/view/1186
+
+matriz = [[] for _ in range(12)]
+
+operacao = input()
+contador = 12
+total = 0
+tam = 0
+for linha in matriz:
+    for _ in range(12):
+        pos = float(input())
+        linha.append(pos)
+    if contador > 0:
+        parte_linha = linha[contador:]
+        soma_linha = sum(parte_linha)
+        tam += len(parte_linha)
+        total += soma_linha
+    contador -= 1
+if operacao == 'M':
+    total /= tam
+print(f'{total:.1f}')
