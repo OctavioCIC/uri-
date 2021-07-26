26c38023ef6a54c8d05f311cf6bcd7077fae28f2
octav1oaugusto <octavioaugusto.cs@hotmail.com>
2021-07-26 20:15:51 -0300

    Adicionar novo exercício

---
+++ 1-Beginner/1189 - Left Area.py
@@ -0,0 +1,26 @@
+# https://www.urionlinejudge.com.br/judge/en/problems/view/1189
+
+matriz = [[] for _ in range(12)]
+
+operacao = input()
+contador = 0
+total = 0
+tam = 0
+for linha in matriz:
+    for _ in range(12):
+        pos = float(input())
+        linha.append(pos)
+    if contador <= 5:
+        parte_linha = linha[:contador]
+        soma_linha = sum(parte_linha)
+        tam += len(parte_linha)
+        total += soma_linha
+    else:
+        parte_linha = linha[:-(contador+1)]
+        soma_linha = sum(parte_linha)
+        tam += len(parte_linha)
+        total += soma_linha
+    contador += 1
+if operacao == 'M':
+    total /= tam
+print(f'{total:.1f}')
