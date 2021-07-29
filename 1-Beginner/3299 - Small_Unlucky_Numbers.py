b2a069994e8d9eae3f17cd8fac58e6be466a4c90
octav1oaugusto <octavioaugusto.cs@hotmail.com>
2021-07-29 18:09:00 -0300

    Adicionar novo exercício

---
+++ 1-Beginner/3299 - Small_Unlucky_Numbers.py
@@ -0,0 +1,8 @@
+# https://www.urionlinejudge.com.br/judge/en/problems/view/3299
+
+numero = int(input())
+
+if '13' in str(numero):
+    print(f'{numero} es de Mala Suerte')
+else:
+    print(f'{numero} No es de Mala Suerte')
